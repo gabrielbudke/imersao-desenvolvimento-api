27cba70a6ab9aa8dd09049006e95ee4356ba0a17
Gabriel Budke de Sousa <gabrielbudke@gmail.com>
2022-03-18 03:07:43 +0000

    :sparkles: Estudando sobre Event Emitter

---
+++ modulo-01/events/index.js
@@ -0,0 +1,19 @@
+const EventEmitter = require("events");
+class MyEmitter extends EventEmitter { }
+
+const myEmitter = new MyEmitter();
+const eventName = "user:click";
+
+myEmitter.on(eventName, function (click) {
+   console.log("um usuário clicou", click);
+});
+
+let count = 0;
+setInterval(() => {
+   myEmitter.emit(eventName, `clicou no ok ${count++}`);
+}, 1000);
+
+const stdin = process.openStdin();
+stdin.addListener("data", function (value) {
+   console.log(`Você digitou: ${value.toString().trim()}`);
+});
