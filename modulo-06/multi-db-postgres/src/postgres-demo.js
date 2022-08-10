bbc11c1b8d9d01a7e220268b42060aa8884108e6
Gabriel Budke de Sousa <gabrielbudke@gmail.com>
2022-08-10 04:02:14 +0000

    :sparkles: Estudando e aplicando o Sequelize no projeto

---
+++ modulo-06/multi-db-postgres/src/postgres-demo.js
@@ -0,0 +1,46 @@
+const { Sequelize, DataTypes } = require("sequelize");
+const sequelize = new Sequelize("heroes", "admin", "admin", {
+    host: "localhost",
+    dialect: "postgres",
+    quoteIdentifiers: false
+});
+
+async function main() {
+    const Heroes = sequelize.define("heroes", {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+            allowNull: false
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        power: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+    }, {
+        tableName: "heroes",
+        freezeTableName: false,
+        timestamps: false
+    });
+
+    /**
+     * Ao definir um model, nós estamos dizendo ao Sequelize algumas coisas sobre a tabela no banco de dados.
+     * Mas, e se a tabela não existir ou até mesmo existir porém as colunas são diferentes? 
+     * 
+     * É aqui que entra a sincronização. O model é sincrozinado através do método model.sync()
+     */
+    await Heroes.sync();
+
+    /**
+     * Realizando uma consulta utilizando o model criado: SELECT * FROM heroes;
+     */
+    const heroes = await Heroes.findAll({ raw: true });
+    console.log("[heroes]", heroes);
+
+}
+
+main();
